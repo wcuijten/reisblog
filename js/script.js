8e350233554d3bd339dff05ae038d69e51c22728
wcuijten <wcuijten@reacollege.nl>
2024-07-10 10:45:46 +0200

    aanpassingen

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ const toggle = document.querySelector(".toggle");
 const menu = document.querySelector(".nav-menu");
 
 /* Toggle mobile menu */
-
 function toggleMenu() {
     if (menu.classList.contains("active")) {
         menu.classList.remove("active");
@@ -24,3 +23,36 @@ toggle.addEventListener("click", toggleMenu, false);
 
 // Add the Dropdown Menu Flexbox Functionality with JavaScript
 const items = document.querySelectorAll(".item");
+/* Event Listener */
+toggle.addEventListener("click", toggleMenu, false);
+
+
+/* Activate Submenu */
+// function toggleItem(e) {
+//     if (this.classList.contains("submenu-active")) {
+//         e.stopPropagation();
+//         this.classList.remove("submenu-active");
+//     } else if (menu.querySelector(":scope > .submenu-active")) {
+//         menu.querySelector(":scope > .submenu-active").classList.remove("submenu-active"); 
+//         this.classList.add("submenu-active");
+//     } else {
+//         this.classList.add("submenu-active");
+//     }
+// }
+
+/* Event Listeners */
+// for (let item of items) {
+//     if (item.querySelector(".submenu")) {
+//         item.addEventListener("click", toggleItem, false);
+//         item.addEventListener("keypress", toggleItem, false);
+//     }   
+// }
+/* Close Submenu From Anywhere */
+function closeSubmenu(e) {
+    if (menu.querySelector(".submenu-active")) {
+        let isClickInside = menu.querySelector(".submenu-active").contains(e.target);
+        if (!isClickInside && menu.querySelector(".submenu-active")) {
+        menu.querySelector(".submenu-active").classList.remove("submenu-active");
+        }
+    }
+}
